ad5796a52d9e264122ed8bca8f3d0b659bf8014d
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-23 19:33:31 -0700

    create first migration

---
+++ api/data/migrations/20210724015421_create-tables.js
@@ -0,0 +1,28 @@
+exports.up = async (knex) => {
+  await knex.schema
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username", 50).unique().notNullable();
+      users.string("password", 50).notNullable();
+      users.string("phone_number", 20).notNullable();
+    })
+    .createTable("plants", (plants) => {
+      plants.increments("plant_id");
+      plants.string("nickname", 50).unique().notNullable();
+      plants.string("species", 100).notNullable();
+      plants.text("water_frequency", 250).notNullable();
+      plants.string("img_url");
+      plants
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists("plants").dropTableIfExists("users");
+};
