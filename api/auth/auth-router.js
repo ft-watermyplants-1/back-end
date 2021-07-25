6a3681b183f17ae1ba52d6539386f4572e76b1c5
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-24 21:23:14 -0700

    create register endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 const Users = require("../users/users-model");
 const bcrypt = require("bcryptjs");
+const tokenBuilder = require("./token-builder");
 
-router.post("/", (req, res, next) => {
-  Users.add(req.body)
-    .then((user) => {
-      res.status(201).json(user);
+router.post("/register", (req, res, next) => {
+  let user = req.body;
+  const rounds = process.env.BCRYPT_ROUNDS || 8;
+  const hash = bcrypt.hashSync(user.password, rounds);
+  user.password = hash;
+
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json({ message: `Welcome ${saved.username}` });
     })
     .catch(next);
 });
