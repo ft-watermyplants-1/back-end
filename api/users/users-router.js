e4a7166a9f1d48cc99645a8833c2e37464563d26
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-26 11:42:16 -0700

    create checkUserPayload middleware

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Users = require("./users-model");
-const { checkUserExists } = require("./users-middleware");
+const { checkUserExists, checkUserPayload } = require("./users-middleware");
 
 router.get("/", (req, res, next) => {
   Users.findAll()
@@ -18,7 +18,7 @@ router.get("/:user_id", checkUserExists, (req, res, next) => {
     .catch(next);
 });
 
-router.put("/:user_id", checkUserExists, (req, res, next) => {
+router.put("/:user_id", checkUserExists, checkUserPayload, (req, res, next) => {
   Users.update(req.params.user_id, req.body)
     .then((user) => {
       res.status(200).json(user);
