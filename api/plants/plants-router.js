82e0882d5dc1dd34c88d4149b5a86da8280083df
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-29 15:45:19 -0700

    disables checkUserExists middleware

--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -6,7 +6,7 @@ const {
   validatePlantPayload,
 } = require("./plants-middleware");
 
-router.get("/", checkUserExists, (req, res, next) => {
+router.get("/", (req, res, next) => {
   Plants.findAll(req.decodedToken.subject)
     .then((plants) => {
       res.status(200).json(plants);
@@ -14,20 +14,15 @@ router.get("/", checkUserExists, (req, res, next) => {
     .catch(next);
 });
 
-router.get(
-  "/:plant_id",
-  checkUserExists,
-  checkPlantExists,
-  (req, res, next) => {
-    Plants.findById(req.decodedToken.subject, req.params.plant_id)
-      .then((plant) => {
-        res.status(200).json(plant);
-      })
-      .catch(next);
-  }
-);
+router.get("/:plant_id", checkPlantExists, (req, res, next) => {
+  Plants.findById(req.decodedToken.subject, req.params.plant_id)
+    .then((plant) => {
+      res.status(200).json(plant);
+    })
+    .catch(next);
+});
 
-router.post("/", checkUserExists, validatePlantPayload, (req, res, next) => {
+router.post("/", validatePlantPayload, (req, res, next) => {
   Plants.add(req.decodedToken.subject, req.body)
     .then((plant) => {
       res.status(201).json(plant);
@@ -37,7 +32,6 @@ router.post("/", checkUserExists, validatePlantPayload, (req, res, next) => {
 
 router.put(
   "/:plant_id",
-  checkUserExists,
   checkPlantExists,
   validatePlantPayload,
   (req, res, next) => {
@@ -49,17 +43,12 @@ router.put(
   }
 );
 
-router.delete(
-  "/:plant_id",
-  checkUserExists,
-  checkPlantExists,
-  (req, res, next) => {
-    Plants.remove(req.params.plant_id)
-      .then((plant) => {
-        res.status(200).json(plant);
-      })
-      .catch(next);
-  }
-);
+router.delete("/:plant_id", checkPlantExists, (req, res, next) => {
+  Plants.remove(req.params.plant_id)
+    .then((plant) => {
+      res.status(200).json(plant);
+    })
+    .catch(next);
+});
 
 module.exports = router;
